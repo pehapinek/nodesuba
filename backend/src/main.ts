4eef77dda068e4d2dc4b71899e1dda0fc10de9dc
pehapinek <pehapinek@protonmail.com>
2022-06-30 17:19:19 +0200

    initialized swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('nodesuba')
+    .setDescription('nodesuba imageboard engine')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
